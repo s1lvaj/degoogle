e42fc89429db82d9e9561a812ec38e81ac96c00e
Silva <jmss.silva@protonmail.com>
2025-11-16 17:41:18 +0000

    Initial code to send email with latest news.

---
+++ rss_news.py
@@ -0,0 +1,101 @@
+import urllib.request
+import json
+import time
+import smtplib
+from email.mime.text import MIMEText
+
+"""
+...
+"""
+
+def get_news(
+        rss_url: str,
+        title: str,
+        body: str,
+):
+    """
+    Get information from the news source, and form a message to be sent.
+
+    Args:
+        rss_url: String of the news rss website whose content you want.
+        title: String of this section for the email.
+        body: String with the current body of the email, to which the new information will be appended.
+    
+    Returns:
+        body: String with the updated body of the email.
+    """
+
+    try:
+        # Compute 24h10min ago in RFC3339 format
+        published_after = time.strftime(
+            "%Y-%m-%dT%H:%M:%SZ", time.gmtime(time.time() - (24 * 60 * 60) - (10 * 60))
+        )
+    except Exception as e:
+        body += f"{e}"
+
+    new_body = ""
+
+    try:
+        response = json.load(urllib.request.urlopen(rss_url))
+        content = ""  # GET THE CONTENT
+        new_body += f"{content}\n"
+    except:
+        pass
+    
+    if new_body != "":  # there's information to be added
+        body += f"{title}:\n{new_body}\n"  # extra blank line at the end
+
+    print("Done " + title)
+
+    return body
+
+
+def send_email(
+        body: str,
+        sender_email: str,
+        sender_password: str,
+        receiver_email: str,
+        ):
+    
+    """
+    Sends an email with the latest news.
+
+    Args:
+        body: Body of the email to be sent.
+        sender_email: String with the email of the sender.
+        sender_password: String with the password of the sender.
+        receiver_email: String with the email of the receiver.
+    
+    Returns:
+        None.
+    """
+    
+    msg = MIMEText(body)
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = 'The Latest News'
+
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 587) as server:
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+    
+    return
+
+
+if __name__ == '__main__':
+
+    SENDER_EMAIL = ""
+    SENDER_PASSWORD = ""
+    RECEIVER_EMAIL = ""
+
+    body = ""
+    body = get_news('https://www.rtp.pt/noticias/rss/pais', 'RTP Noticias Portugal', body)  # example of the rss of rtp, a portuguese news source
+    body = get_news('https://www.rtp.pt/noticias/rss/mundo', 'RTP Noticias Mundo', body)
+    body = get_news('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml', 'RTP Noticias Portugal', body)  # example of the rss of another news source
+    body = get_news('https://rss.nytimes.com/services/xml/rss/nyt/World.xml', 'RTP Noticias Mundo', body)
